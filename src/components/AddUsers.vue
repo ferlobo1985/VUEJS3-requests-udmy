444f1c8860afa555e2ddb9fac6bad817dfd376c8
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-30 18:56:15 -0300

    04-Posting

--- src/components/AddUsers.vue
+++ src/components/AddUsers.vue
@@ -1,6 +1,10 @@
 <template>
     <h2>Add users</h2>
 
+    <div v-show="loading">
+        <app-loader></app-loader>
+    </div>
+    <div v-show="!loading">
         <div class="form-group">
             <label>Name</label>
             <input class="form-control" type="text" v-model="user.name">
@@ -12,12 +16,16 @@
         </div>
 
         <button class="btn btn-primary" @click="submit">Submit</button>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
         data(){
             return{
+                loading:false,
                 user:{ 
                     name:'',
                     lastname:''
@@ -26,7 +34,30 @@ export default {
         },
         methods:{
             submit(){
-                console.log(this.user)
+                this.loading = true;
+                axios({
+                    method:'post',
+                    url:'http://localhost:3004/users',
+                    data: this.user
+                })
+                .then( () => {
+                      this.$toast.success('The user has been added',{
+                        position:'bottom',
+                        duration:false
+                      });
+                })
+                .catch( error => {
+                   this.$toast.error(error,{
+                        position:'bottom',
+                        duration:false
+                      });
+                })
+                .finally(()=>{
+                    this.loading = false;  
+                })
+
+
+
             }
         }
 }
