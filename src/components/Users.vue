8ae72f1c456e956443bc667e28466a5f08108510
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-30 18:11:03 -0300

    02-GET posts

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,24 +1,54 @@
 <template>
-<div class="row">
+    <div class="row">
 
-     <div class="col-auto mb-4">
-        <div class="card" style="width: 14rem;">
-            <img class="card-img-top" src="https://placebeard.it/300/300"  alt="Card image cap">
-            <div class="card-body">
-                <div class="card-text">
-                    <strong>Name:</strong> Francis
-                </div>
-                <div class="card-text">
-                    <strong>Lastname:</strong> Jones
+        <div 
+            class="col-auto mb-4"
+            v-for="(user) in users"
+            :key="user.id"
+        >
+            <div class="card" style="width: 14rem;">
+                <img 
+                    class="card-img-top" 
+                    :src="'https://placebeard.it/300/300?'+ user.id"  
+                    alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-text">
+                        <strong>Name:</strong> {{ user.name }}
+                    </div>
+                    <div class="card-text">
+                        <strong>Lastname:</strong> {{ user.lastname }}
+                    </div>
                 </div>
             </div>
         </div>
-     </div>
 
-</div>
-   
+    </div>
 </template>
 
 
 <script>
+    import axios from 'axios';
+
+    export default {
+        data(){
+            return {
+                users:[]
+            }
+        },
+        methods:{
+            loadUsers(){
+                axios.get('http://localhost:3004/users')
+                .then( response => {
+                    this.users = response.data
+                })
+                .catch( error => {
+                    console.log( error )
+                })
+            }  
+        },
+        mounted(){
+          this.loadUsers()
+        }
+    }
+
 </script>
